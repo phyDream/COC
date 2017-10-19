4b6e7d5eec9746a62b513ee113845803c39f5e81
phydream <13219110554@163.com>
2017-10-19 21:18:34 +0800

    2017-10-19 21:18:29

---
+++ app/src/main/java/com/cdlixin/coc/ui/sd/activity/TestActivity.java
@@ -0,0 +1,15 @@
+package com.cdlixin.coc.ui.sd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cdlixin.coc.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
---
+++ app/src/main/java/com/cdlixin/coc/ui/sd/widght/TestView.java
@@ -0,0 +1,25 @@
+package com.cdlixin.coc.ui.sd.widght;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 蒲弘宇 on 2017/10/18.
+ */
+
+public class TestView extends View {
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
